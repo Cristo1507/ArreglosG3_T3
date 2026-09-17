0a57c315047566ef90f7c8200285a4dda3f01f20
Cristo1507 <25030446@itesa.edu.mx>
2026-09-17 07:28:11 -0600

    Version_02: Metodo llenar arreglo

--- Arreglos.UI.Consola/Program.cs
+++ Arreglos.UI.Consola/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Arreglos.Logica;
+
+Console.WriteLine("Hello, World!");
+
+
+MiArreglo oMyArreglo = new(10000);
+
+oMyArreglo.Llenar(5, 50);
+
+//oMyArreglo.ToString();
+
+Console.WriteLine(oMyArreglo);
+Console.ReadKey();
